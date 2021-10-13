aa3ca4eb458fbf5b3295266a0f074bf55e63e44a
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2021-10-13 16:20:10 -0300

    correcao da indentacao

--- src/components/CardFounders.js
+++ src/components/CardFounders.js
@@ -11,12 +11,12 @@ function CardFounders(props) {
             <div className={css.content}>
                 <div className={css.details}>
                     <h4>{props.name}<br/><span>{props.role}</span></h4>
-                        <ul className={css.social_icons}>
-                            <li><a href={props.social_icons.face}><i className={props.social_icons.face === '' ? noSocial : "fab fa-facebook-f"}></i></a></li>
-                            <li><a href={props.social_icons.twitter}><i className={props.social_icons.twitter === '' ? noSocial : "fab fa-twitter"}></i></a></li>
-                            <li><a href={props.social_icons.linkedin}><i className={props.social_icons.linkedin === '' ? noSocial : "fab fa-linkedin-in"}></i></a></li>
-                            <li><a href={props.social_icons.insta}><i className={props.social_icons.insta === '' ? noSocial : "fab fa-instagram"}></i></a></li>
-                        </ul>
+                    <ul className={css.social_icons}>
+                        <li><a href={props.social_icons.face}><i className={props.social_icons.face === '' ? noSocial : "fab fa-facebook-f"}></i></a></li>
+                        <li><a href={props.social_icons.twitter}><i className={props.social_icons.twitter === '' ? noSocial : "fab fa-twitter"}></i></a></li>
+                        <li><a href={props.social_icons.linkedin}><i className={props.social_icons.linkedin === '' ? noSocial : "fab fa-linkedin-in"}></i></a></li>
+                        <li><a href={props.social_icons.insta}><i className={props.social_icons.insta === '' ? noSocial : "fab fa-instagram"}></i></a></li>
+                    </ul>
                 </div>
             </div>
         </div>

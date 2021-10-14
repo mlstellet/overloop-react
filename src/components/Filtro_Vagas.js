f1f56951b3bb279476e55956fffd69682dbad536
Tais Bruno Rabelo <taisbruno@id.uff.br>
2021-10-14 17:02:02 -0300

    Componentizando Filtro da Página de Vagas

---
+++ src/components/Filtro_Vagas.js
@@ -0,0 +1,43 @@
+import css from '../assets/css/filtro.module.css'
+import lupa from '../assets/img/icon-search.png'
+import mala from '../assets/img/vaga-mala-icon.svg'
+import localizacao from '../assets/img/vagas-localizacao-icon.svg'
+
+function Filtro() {
+    return(
+    <section className={css.top}>
+        <h1>Vagas</h1>
+        <hr />
+        <p>Encontre seu emprego dos sonhos e dê match!</p>
+        <div className={css.formulario}>
+            <div className={css.input_cargo}>
+                <label for="cargo-empresa">
+                    <img className={css.icon} src={mala} alt="Ícone mala" />
+                </label>
+                <input list="cargo-empresas" name="cargo-empresa" id="cargo-empresa" placeholder="Filtrar por cargo" />
+                <datalist id="cargo-empresas">
+                  <option value="DevOps" />
+                  <option value="Backend" />
+                  <option value="Fullstack" />
+                  <option value="TI" selected />
+                </datalist>
+            </div>
+                
+            <div className={css.input_localizacao}>
+                <label for="localizacao">
+                    <img className={css.icon} src={localizacao} alt="Ícone localização" />
+                </label>
+                <input id={css.localizacao} type="text" placeholder="Filtrar por localização" />
+            </div>
+               
+                
+            <button id={css.search}>
+                <img id={css.lupa} src={lupa} alt="Ícone lupa" />
+                <span>Pesquisar</span>
+            </button>
+        </div>
+    </section>
+    )
+}
+
+export default Filtro
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Inspiracoes from './pages/Inspiracoes';
+import Vagas from './pages/Vagas';
 
 function Routes() {
     return (
@@ -8,6 +9,7 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={Home}/>
                 <Route path="/inspiracoes" component={Inspiracoes}/>
+                <Route path="/vagas" component={Vagas}/>
             </Switch>
         </BrowserRouter>
     )

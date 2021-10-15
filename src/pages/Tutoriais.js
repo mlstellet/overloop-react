c0347e0cbd0b6c8d371bcea10fa58c7ebbc88d95
Maria Luiza Stellet <marialuizastellet@gmail.com>
2021-10-15 14:29:23 -0300

    Inicializando a página de Tutoriais

---
+++ src/pages/Tutoriais.js
@@ -0,0 +1,11 @@
+import CardTutoriais  from "../components/CardsTutoriais";
+import css from '../assets/css/tutoriais.module.css';
+function Tutoriais() {
+    return (
+        <section className={css.tutoriais}>
+            <CardTutoriais />
+        </section>
+    );
+}
+
+export default Tutoriais; 
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Inspiracoes from './pages/Inspiracoes';
+import Tutoriais from './pages/Tutoriais';
 
 function Routes() {
     return (
@@ -8,6 +9,7 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={Home}/>
                 <Route path="/inspiracoes" component={Inspiracoes}/>
+                <Route path="/tutoriais" component={Tutoriais}/>
             </Switch>
         </BrowserRouter>
     )

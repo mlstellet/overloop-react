6f5596be0a91dd9b6704f8edd92a3a793d0092b6
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2021-10-14 22:30:37 -0300

    add: banner em tutoriais

---
+++ src/pages/Tutoriais.js
@@ -0,0 +1,17 @@
+import Banner from "../components/Banner"
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+
+function Tutoriais() {
+    return (
+        <>
+            <Header fundo="transparente" />
+
+            <Banner title="Tutoriais" url="/img/tutoriais-banner.png" />
+
+            <Footer />
+        </>
+    )
+}
+
+export default Tutoriais

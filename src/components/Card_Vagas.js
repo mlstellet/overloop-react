3aa8406250c576955425635672a87c867dd40646
Tais Bruno Rabelo <taisbruno@id.uff.br>
2021-10-19 16:51:52 -0300

    Api Jooble React

--- src/components/Card_Vagas.js
+++ src/components/Card_Vagas.js
@@ -1,120 +1,42 @@
 import css from '../assets/css/cardVagas.module.css'
+import React, { useEffect, useState } from "react";
+import api from "./vagas";
 
-function Cards_Vagas() {
-    return(
-        <section className={css.content}>
-        <article className={css.job}>
-            <div className={css.job_info}>
-                <div className={css.text}>
-                    <a href=""><h2 className={css.tipo}>Tipo de vaga</h2></a>
-                <p className={css.descricao}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, amet est voluptas ducimus perspiciatis esse a quos rerum aspernatur necessitatibus nesciunt.
-                </p>
-                <span className={css.location}>Lugar</span>
+export default function App() {
+    const api_key = "e03cb2fc-44fa-493f-b3f0-582fb8f39288";
+    const [user, setUser] = useState([]);
+    const [data, setData] = useState({ keywords: 'it' });
+    const [show, setShow] = useState(false)
+    useEffect(() => {
+        api
+          .post("/api/"+api_key, data)
+          .then((response) => setUser(response.data.jobs))
+          .then(() => setShow(true))
+          .catch((err) => {
+            console.error("ops! ocorreu um erro" + err);
+          });
+      }, []);
+
+    return (
+        <>
+        {show && (
+            <>
+            {user.map(single=>(
+            <section className={css.content}>
+            <article className={css.job}>
+                <div className={css.job_info}>
+                    <div className={css.text}>
+                        <a href={single.link} target='_blank'><h2 className={css.tipo}>{single.title}</h2></a>
+                    <p className={css.descricao} dangerouslySetInnerHTML={{__html: single.snippet}} /> 
+                    <span className={css.location}>{single.location}</span>
+                    </div>
                 </div>
-            </div>
-        </article>
-
-        <article className={css.job}>
-            <div className={css.job_info}>
-                <div className={css.text}>
-                    <a href=""><h2 className={css.tipo}>Tipo de vaga</h2></a>
-                <p className={css.descricao}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, amet est voluptas ducimus perspiciatis esse a quos rerum aspernatur necessitatibus nesciunt.
-                </p>
-                <span className={css.location}>Lugar</span>
-                </div>
-            </div>
-        </article>
-
-        <article className={css.job}>
-            <div className={css.job_info}>
-                <div className={css.text}>
-                    <a href=""><h2 className={css.tipo}>Tipo de vaga</h2></a>
-                <p className={css.descricao}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, amet est voluptas ducimus perspiciatis esse a quos rerum aspernatur necessitatibus nesciunt.
-                </p>
-                <span className={css.location}>Lugar</span>
-                </div>
-            </div>
-        </article>
-
-        <article className={css.job}>
-            <div className={css.job_info}>
-                <div className={css.text}>
-                    <a href=""><h2 className={css.tipo}>Tipo de vaga</h2></a>
-                <p className={css.descricao}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, amet est voluptas ducimus perspiciatis esse a quos rerum aspernatur necessitatibus nesciunt.
-                </p>
-                <span className={css.location}>Lugar</span>
-                </div>
-            </div>
-        </article>
-
-        <article className={css.job}>
-            <div className={css.job_info}>
-                <div className={css.text}>
-                    <a href=""><h2 className={css.tipo}>Tipo de vaga</h2></a>
-                <p className={css.descricao}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, amet est voluptas ducimus perspiciatis esse a quos rerum aspernatur necessitatibus nesciunt.
-                </p>
-                <span className={css.location}>Lugar</span>
-                </div>
-            </div>
-        </article>
-
-        <article className={css.job}>
-            <div className={css.job_info}>
-                <div className={css.text}>
-                    <a href=""><h2 className={css.tipo}>Tipo de vaga</h2></a>
-                <p className={css.descricao}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, amet est voluptas ducimus perspiciatis esse a quos rerum aspernatur necessitatibus nesciunt.
-                </p>
-                <span className={css.location}>Lugar</span>
-                </div>
-            </div>
-        </article>
-
-        <article className={css.job}>
-            <div className={css.job_info}>
-                <div className={css.text}>
-                    <a href=""><h2 className={css.tipo}>Tipo de vaga</h2></a>
-                <p className={css.descricao}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, amet est voluptas ducimus perspiciatis esse a quos rerum aspernatur necessitatibus nesciunt.
-                </p>
-                <span className={css.location}>Lugar</span>
-                </div>
-            </div>
-        </article>
-
-        <article className={css.job}>
-            <div className={css.job_info}>
-                <div className={css.text}>
-                    <a href=""><h2 className={css.tipo}>Tipo de vaga</h2></a>
-                <p className={css.descricao}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, amet est voluptas ducimus perspiciatis esse a quos rerum aspernatur necessitatibus nesciunt.
-                </p>
-                <span className={css.location}>Lugar</span>
-                </div>
-            </div>
-        </article>
-
-        <article className={css.job}>
-            <div className={css.job_info}>
-                <div className={css.text}>
-                    <a href=""><h2 className={css.tipo}>Tipo de vaga</h2></a>
-                <p className={css.descricao}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, amet est voluptas ducimus perspiciatis esse a quos rerum aspernatur necessitatibus nesciunt.
-                </p>
-                <span className={css.location}>Lugar</span>
-                </div>
-            </div>
-        </article>
-
-    </section>
-
-)
+            </article>
+            </section>
+        ))}
+            </>
+        )}
+        </>
+    )
 }
-
-export default Cards_Vagas
 

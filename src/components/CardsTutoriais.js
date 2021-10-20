f4736af0767b1af1e7b1289d73e579bc05e04db7
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2021-10-20 16:03:10 -0300

    paginacao Tutoriais

--- src/components/CardsTutoriais.js
+++ src/components/CardsTutoriais.js
@@ -1,34 +1,27 @@
-import { array } from "prop-types";
 import css from "../assets/css/cardTutoriais.module.css";
-import data from '../data.json'
 
-function CardTutoriais() {
-    let lista = [1, 2, 3, 4]
+function CardTutoriais({data}) {
     return (
         <div> 
-                {
-                    data.map(item => {
-                        return (
-                            <article>
-                                <div className={css.container}>
-                                    <div className={css.logo}>
-                                        <img src={item.logo} alt="" />
-                                    </div>
-                                    <div className={css.texto}>
-                                        <h3 key={item.chave}>{item.nome}</h3>
-                                        <p key={item.chave}>{item.descricao}</p>
-                                        <ul>
-                                            <li><a href={item.link1} target="_blank">{item.nomeLink1}</a></li>
-                                            <li><a href={item.link2} target="_blank">{item.nomeLink2}</a></li>
-                                        </ul>
-                                    </div>
-
-                                </div>
-                            </article> 
-                        );
-                        
-                    })
-                }
+            {data.map(item => {
+                return (
+                    <article>
+                        <div className={css.container}>
+                            <div className={css.logo}>
+                                <img src={item.logo} alt="" />
+                            </div>
+                            <div className={css.texto}>
+                                <h3 key={item.chave}>{item.nome}</h3>
+                                <p key={item.chave}>{item.descricao}</p>
+                                <ul>
+                                    <li><a href={item.link1} target="_blank">{item.nomeLink1}</a></li>
+                                    <li><a href={item.link2} target="_blank">{item.nomeLink2}</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </article> 
+                );
+            })}
         </div>
     );
 }

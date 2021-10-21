628ff8b56257ca6bb6f5af194c73b8bf089125d4
Maria Luiza Stellet <marialuizastellet@gmail.com>
2021-10-21 11:57:15 -0300

    Página NPS-enviado

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Inspiracoes from './pages/Inspiracoes';
 import Vagas from './pages/Vagas';
 import Tutoriais from './pages/Tutoriais';
 import Nps from './pages/Nps';
+import NpsEnviado from './pages/NpsEnviado';
 
 function Routes() {
     return (
@@ -14,6 +15,7 @@ function Routes() {
                 <Route path="/vagas" component={Vagas}/>
                 <Route path="/tutoriais" component={Tutoriais}/>
                 <Route path="/nps" component={Nps} />
+                <Route path="/npsEnviado" component={NpsEnviado} />
             </Switch>
         // </BrowserRouter>
     )

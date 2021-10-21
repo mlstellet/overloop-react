205c48324a3a2333e73725504bde82f005bdcd5d
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2021-10-21 11:16:16 -0300

    pequenos ajustes

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,16 +1,15 @@
 import styled from "styled-components"
 
 const Container = styled.div`
-    max-width: calc(50vw);
-    width: 620px;
+    max-width: 620px;
+    min-width: 275px;
+    width: calc(50vw);
 `
 
 export default function Text(props) {
     return (
-        // <div className="text">
         <Container>
             {props.children}
         </Container>
-        // </div>
     )
 }

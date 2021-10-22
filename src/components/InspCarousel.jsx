6bb58f76e96b5fb56ad7fd2829186ed08a9f0362
khiaraShantala <khiara.shantala@gmail.com>
2021-10-22 11:11:19 -0300

    add background carousel

--- src/components/InspCarousel.jsx
+++ src/components/InspCarousel.jsx
@@ -1,33 +1,29 @@
-import "../assets/css/carousel.css"
+import "../assets/css/carousel.css";
 
 function Carrossel(props) {
-  
-    
-    return (
-        <>
-            <div className="profissoes"> 
-                <div className="subtitulo">
-                    <h2>{props.area}</h2>
-                </div>
+  return (
+    <>
+      <div className="profissoes">
+        <div className="subtitulo">
+          <h2>{props.area}</h2>
+        </div>
 
-                <div className="info">
-                    <div className="fotos">
-                    <img src={props.avatar} alt=""/>
-                    </div>
+        <div className="info">
+          <div className="fotos">
+            <img src={props.avatar} alt="" />
+          </div>
 
-                    <div className="texto">
-                        <div className="titulo">
-                            <h3>{props.name}</h3>
-                            <h4>{props.role}</h4>
-                        </div>
-
-                        <p>{props.description}</p>
-                    </div>
-                </div>
-            </div> 
-        </>
-    );
-  
+          <div className="texto">
+            <div className="titulo">
+              <h3>{props.name}</h3>
+              <h4>{props.role}</h4>
+            </div>
+            <p>{props.description}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Carrossel;
